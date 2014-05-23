e196d3d4a6e5e7dbf685c4927a6eb724f49cd94b
Sergey <mazi095@gmail.com>
2014-05-23 02:10:18 -0400

    commit to pegas

---
+++ src/Task/TestBundle/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Task\TestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Task\TestBundle\Entity\Search;
+// these import the "@Route" and "@Template" annotations
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+
+
+class DefaultController extends Controller
+{
+  /**
+   * @Route("/", name="_task_index")
+   * @Template()
+   */
+  public function indexAction(){
+    return $this->render('TaskTestBundle:Default:index.html.twig');
+  }
+   /**
+   * @Route("/userform", name="task_pegas")
+   * @Template()
+   */
+  public function taskAction(){
+    $request = $this->get('request');
+    $search = new Search();
+    $form = $this->createFormBuilder($search)
+      ->add('expresion', 'text')
+      ->getForm();
+    if ($request->query->get('form')) {
+      $form->bind($request);
+      if ($form->isValid()) {
+      }
+    }
+    return $this->render('TaskTestBundle:Default:search.html.twig', array(
+      'form' => $form->createView(),
+      'data' => $search->getGeneralResult()
+    ));
+  }
+}
---
+++ src/Task/TestBundle/TaskTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Task\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TaskTestBundle extends Bundle
+{
+}
